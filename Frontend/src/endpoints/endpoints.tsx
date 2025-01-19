df43fc62b2750217e3ff498b5b9fdd5ac8d0a50f
Shokudaikiri <solomonkaniaru154@gmail.com>
2025-01-19 16:11:18 +0300

    Update: codebase structure

--- Frontend/src/endpoints/endpoints.tsx
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const getUsers = async () => {
-    const response = await axios.get('http://localhost:3001/users'); 
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-
-}
-export default getUsers;
